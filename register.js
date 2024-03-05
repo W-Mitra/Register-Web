95e3a0a923ec1c0902599cab2929bba96dfe9701
W-Mitra <wendelynmitra900@gmail.com>
2024-03-05 14:58:12 -0800

    my register js

---
+++ register.js
@@ -0,0 +1,53 @@
+console.log("Hello World!");
+console.error("ERROR HAPPENED!!!");
+console.warn("Warning!!!");
+
+// Variables
+// string = "Words" , 'woowoo' , '123'
+// number = 123, 434, 2398.12
+// boolean = true, false
+// object = ["apple", "banana"], [1,2,3,4,5]
+        //["apple", 3, true]
+
+// type of
+// console.log(typeof(["apple", "banana"]))
+
+// Declaration of Variablesg
+
+let name = "Wendy";
+lastname = 'Mitra';
+let age = 19;
+let isProf = true;
+let hobbies = ["playing online games", "watching movies"];
+run = function () {
+    console.log("Im running");
+}
+console.log(name,age,isProf,hobbies);
+console.log(hobbies);
+console.log("My name is" + name +" " + lastname);
+console.log(`My name is ${name} ${lastname}`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
